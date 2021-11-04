527f281d857797847a94f03117f047dc0743fb5b
lersgeeb <banegas.gabriel13@gmail.com>
2021-11-04 14:26:42 -0600

    categories view

--- dw-client/src/app/core/views/categories/categories.component.ts
+++ dw-client/src/app/core/views/categories/categories.component.ts
@@ -6,6 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
+  categories = [
+    {
+      name: 'Restaurante',
+      icon: 'restaurant'
+    },
+    {
+      name: 'Supermercado',
+      icon: 'market'
+    },
+    {
+      name: 'Farmacia',
+      icon: 'drugStore'
+    },
+    {
+      name: 'Fiesta',
+      icon: 'party'
+    },
+    {
+      name: 'Ferretería',
+      icon: 'tool'
+    },
+    {
+      name: 'Joyería',
+      icon: 'jewel'
+    },
+    {
+      name: 'Café',
+      icon: 'coffe'
+    },
+    {
+      name: 'Fitness',
+      icon: 'exercise'
+    },
+
+  ]
+
 
   constructor() { }
 
